5cd185a7b7324252b915324e735788bec0be53a5
Louis MOUHAT <louis.mouhat@gmail.com>
2021-04-12 14:52:23 +0200

    scroll to scheduled video

--- youtube_uploader_selenium/__init__.py
+++ youtube_uploader_selenium/__init__.py
@@ -90,6 +90,8 @@ class YouTubeScheduler(YoutubeWorker):
         scheduled_dates = []
         if hover_list is not None and day_list is not None:
             for hoverElement, dayElement in zip(hover_list, day_list):
+                self.browser.driver.execute_script("arguments[0].scrollIntoView(true);", hoverElement)
+                time.sleep(.2)
                 actions = ActionChains(self.browser.driver)
                 actions.move_to_element(hoverElement)
                 actions.perform()
